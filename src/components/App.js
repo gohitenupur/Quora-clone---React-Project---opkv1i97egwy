1427d4597aea3d60e585b924012d543af137bf56
Nupur Gohite <118847362+gohitenupur@users.noreply.github.com>
2023-02-17 01:15:33 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,26 @@
 import React from "react";
 import "../styles/App.css";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import AddQuestion from "./AddQuestion";
 // import RegisterPage from "./RegisterPage";
 // import AddAnswer from "./AddAnswer";
 // import LoginPage from "./LoginPage";
 import Header from "./Header";
-import QAPage from "./QAPage";
+// import QAPage from "./QAPage";
 const App = () => {
   return (
     <div>
-      <Header />
-      {/* <BrowserRouter> */}
-      {/* <Routes> */}
-      {/* <Route path="/" element={<Header />} /> */}
-      {/* <Route path="/AddQuestion" element={<AddQuestion />} /> */}
-      {/* </Routes> */}
-      {/* </BrowserRouter> */}
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          {/* <Route path="/AddQuestion" element={<AddQuestion />} /> */}
+        </Routes>
+      </BrowserRouter>
+      {/* <QAPage /> */}
       {/* <AddQuestion /> */}
       {/* <AddAnswer /> */}
       {/* <LoginPage /> */}
       {/* <RegisterPage /> */}
-      <QAPage />
     </div>
   );
 };
--- src/components/QAPage.js
+++ src/components/QAPage.js
@@ -4,16 +4,14 @@ import QuestionAnswerList from "./QuestionAnswerList";
 
 function QAPage() {
   return (
-    <>
-      <div className="qa-page">
-        <div className="left-panel">
-          <QuestionAnswerList />
-        </div>
-        <div className="right-panel">
-          <QuestionList />
-        </div>
+    <div className="qa-page">
+      <div className="left-panel">
+        <QuestionAnswerList />
       </div>
-    </>
+      <div className="right-panel">
+        <QuestionList />
+      </div>
+    </div>
   );
 }
 
